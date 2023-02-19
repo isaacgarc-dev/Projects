c801ac7a74197f34d2d3536ac24ced5174bd5e41
isaacgarc-dev <124853347+isaacgarc-dev@users.noreply.github.com>
2023-02-18 23:37:49 -0400

    Theme MUi Set

--- SocialApp/client/src/App.js
+++ SocialApp/client/src/App.js
@@ -2,16 +2,27 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import LoginPage from "scenes/loginPage";
 import HomePage from "scenes/homePage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), { mode });
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Router path="/" element={<LoginPage />} />
-          <Router path="/home" element={<HomePage />} />
-          <Router path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Router path="/" element={<LoginPage />} />
+            <Router path="/home" element={<HomePage />} />
+            <Router path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
