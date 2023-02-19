8989279e86b99d8089b53a4fb4694a4b381e4af0
isaacgarc-dev <124853347+isaacgarc-dev@users.noreply.github.com>
2023-02-18 21:04:32 -0400

    Implemented Routing for React Client

--- SocialApp/client/src/App.js
+++ SocialApp/client/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import LoginPage from "scenes/loginPage";
+import HomePage from "scenes/homePage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Router path="/" element={<LoginPage />} />
+          <Router path="/home" element={<HomePage />} />
+          <Router path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
